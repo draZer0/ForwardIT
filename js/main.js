63a38a94149eb0f2587235d0832b22b55fc43c88
drazero <ardkokkel@outlook.com>
2019-11-30 13:10:28 +0100

    main javascript to toggle navigation and active styling for hamburger

---
+++ js/main.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+
+
+  var toggle = document.getElementById("js--menu-toggle");
+
+  toggle.onclick = (event) => {
+    var menu = document.getElementById("js--toggled");
+  	var menuStyle = window.getComputedStyle(menu);
+  	var display = menuStyle.getPropertyValue('display');
+
+    if (display === 'none') {
+      toggle.style.backgroundColor = "gray";
+      menu.style.display = "block";
+    } else {
+      toggle.style.backgroundColor = "transparent";
+      menu.style.display = "none";
+    }
+  }
+
+
+
+
+
+
+}
